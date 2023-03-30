76d625864e160c2b53ebcafb3fcbaf0aad720a4f
vehenjini <tov0821@gmail.com>
2023-03-30 10:33:49 +0900

    🎉 team

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Nav from './component/Nav'
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <div className="Nav">
+        <img src="/logo.png" alt="logo" className="logo" />
+    </div>
+  )
+}
+export default Nav;
